bc933da591376cced191d1f777b26eb735f82b0e
electronobots <electronobots@p3nlhftpg082.shr.prod.phx3.secureserver.net>
2015-04-01 11:17:24 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html>
+
+ <head>
+
+  <title>Stringent Studios &middot; Software Design</title>
+  <meta charset="utf-8" />
+  <link rel="shortcut icon" href="/stringent/favicon.ico" />
+  <link rel="stylesheet" href="/stringent/stringent.css" />
+
+ </head>
+
+ <body>
+
+  <header>
+   <img src="/stringent/stringent-logo.png" />
+   <h1>Stringent Studios</h1>
+   <h2>Software &middot; Design</h2>
+  </header>
+
+  <nav>
+   <ul>
+	 <li><a href="#products">Products</a></li>
+	 <li><a href="#services">Services</a></li>
+	 <li><a href="#contact">Contact</a></li>
+	</ul>
+  </nav>
+
+  <section id="products">
+   <h3>Products</h3>
+   <ul>
+	 <li>Web Apps</li>
+	 <li>iOS Apps</li>
+	 <li>iOS Games</li>
+	</ul>
+  </section>
+
+  <section id="services">
+   <h3>Services</h3>
+   <ul>
+	 <li>iOS Development</li>
+	 <li>Web Design &amp; Deveopment</li>
+	</ul>
+  </section>
+
+  <section id="contact">
+   <h3>Contact</h3>
+	<p>
+	 <b>Stringent Studios</b><br />
+	 <a href="mailto:info@stringentstudios.com">info@stringentstudios.com</a><br />
+	 +1 785.764.0385<br />
+	</p>
+  </section>
+
+  <footer>
+   <p>&copy; copyright <?php echo date("Y"); ?> stringent studios. all rights reserved.</p>
+  </footer>
+
+ </body>
+
+</html>
